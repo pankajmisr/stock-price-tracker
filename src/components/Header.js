88b957843146fe212adbebbb8b68daced99f6b94
Pankaj Mishra <pankajmisr@gmail.com>
2025-04-01 10:51:32 -0400

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Stock Price Tracker</h1>
+      <p>Track your favorite stocks with alerts for significant price changes</p>
+    </div>
+  );
+};
+
+export default Header;
